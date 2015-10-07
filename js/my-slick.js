e9a07489eb05b67fe5ef060c6fd60fcedacb98d6
alo0822 <fangyulo@Fang-Yus-MacBook-Pro.local>
2015-10-06 22:46:15 -0700

    Landing page update
    Fixed header, photo banner, carousel.

---
+++ js/my-slick.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $('.snap-featured-projects').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
